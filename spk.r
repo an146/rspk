0411032c0f8e8077a3712afb81ec657b83f4056b
Anek Novikov <a.novikov@neksoft.ru>
2019-07-16 22:21:59 +0500

    initial commit

---
+++ spk.r
@@ -0,0 +1,9 @@
+library('RcppFaddeeva')
+voigt <- function(xval, xpeak, fG, fL, height) {
+	sigma <- fG / (2 * (2*log(2))^0.5)
+	gamma <- fL / 2
+	z <- complex(real = xval - xpeak, imaginary = gamma) / (sqrt(2) * sigma)
+	peakZ <- complex(imaginary = gamma) / (sqrt(2) * sigma)
+	normFactor <- height / (Re(Faddeeva_w(peakZ)) / (sigma * sqrt(2 * pi)))
+	return(Re(Faddeeva_w(z)) / (sigma * sqrt(2 * pi)) * normFactor)
+}
